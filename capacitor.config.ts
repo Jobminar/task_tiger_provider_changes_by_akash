611565d35d4187493637d31c0528d6f8b1992ef0
moodanilchowan1111 <moodanilchowan1111@gmail.com>
2024-09-30 22:45:38 +0530

    updated webdir file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tasktigersprovider.app',
   appName: 'coolie-no.1-provider',
-  webDir: 'dist'
+  webDir: 'dist/coolie-no.1-provider/browser'
 };
 
 export default config;
